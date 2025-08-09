9f2867aef9bdf2ed4deeee55a50e0138380c319e
ReaverZx <ibrahim.seye0909@gmail.com>
2025-08-09 19:03:16 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,25 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
-// Create HTTP server (just returns OK to any HTTP request)
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("WebSocket relay server is running\n");
+  res.end("THNK Relay server is running\n");
 });
 
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws: WebSocket) => {
-  ws.on("message", (message: WebSocket.RawData) => {
-    const msgStr = message.toString();
-    console.log("Received:", msgStr);
-
+  ws.on("message", (message, isBinary) => {
+    // Broadcast message to all other clients exactly as received
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msgStr);
+        client.send(message, { binary: isBinary });
       }
     });
   });
 
-  ws.send("Connected to THNK relay server");
+  // Optionally, send a small hello (THNK doesn't need this)
+  console.log("New client connected");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
